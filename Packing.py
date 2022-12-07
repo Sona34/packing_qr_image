3c34f5bdb70355c4652e9790a8a6ad1a0ba0ba98
Sona34 <b1019174@fun.ac.jp>
2022-12-07 18:47:59 +0900

    add qrcode-decoder

--- Packing.py
+++ Packing.py
@@ -4,10 +4,14 @@ import cv2
 import numpy as np
 from rectpack import newPacker
 import random
+import time
+import os
+import itertools
 
 from PIL import Image
+from pyzbar.pyzbar import decode 
 
-def get_qrcode_images(image_path = Parameters.WorkSpacePath["qrcode_image_path"]):
+def get_qrcode_info(image_path = Parameters.WorkSpacePath["qrcode_image_path"]):
     
     # get file path
     qrcode_image_files = glob.glob(image_path + "/*.*")
@@ -47,11 +51,12 @@ def placement_qrcode_images(qrcode_images, rect_lists, bin_width, bin_height):
 
 
 if __name__ == "__main__":
+    decode_time = int()
 
-    # 新しいパッカーオブジェクトの生成
+    # get try steps
+    try_steps = Parameters.parameters["try_steps"]
+
     packer = newPacker()
-
-    images, items = get_qrcode_images()
 
     # get bin_shape (width & height)
     bin_width = int(Parameters.parameters["bin_width"])
@@ -59,42 +64,56 @@ if __name__ == "__main__":
 
     # add bin info to packer
     packer.add_bin(bin_width, bin_height, bid=0)
-
-    # add rect info to packer
-    for index, item in enumerate(items):
-        packer.add_rect(item[0], item[1], rid=index)
-
-    # packing calcurate
-    packer.pack()
-
-    rect_lists = list()
-
-    # append rect elements to rect_lists
-    for r in packer.rect_list():
-        # index, x, y, w, h
-        rect_lists.append((r[5], r[1], r[2], r[3], r[4]))
     
-    # Image placement from calculated values
-    output_image = placement_qrcode_images(images, rect_lists, bin_width, bin_height)
-
-    output_image_path = Parameters.WorkSpacePath["output_image_path"]
-    cv2.imwrite(output_image_path, output_image)
+    start_time = time.time()
     
-    
-    # # 計測開始時間
-    # start_time = time.time()
+    # iter loop
+    for i in itertools.count():
+        # update qrcode info
+        qrcode_images, qrcode_items = get_qrcode_info()
 
-    # # 試行回数の取得
-    # try_steps = Parameters.parameters["try_steps"]
+        # add rect info to packer
+        for index, item in enumerate(qrcode_items):
+            packer.add_rect(item[0], item[1], rid=index)
+        
+        # packing
+        packer.pack()
 
-    # for i in range(try_steps):
+        rect_lists = list()
 
-    #     # decode
-    #     decode_start_time = time.time()
-    #     data = decode(Image.open())
-    #     decode_end_time = time.time()
-    
-    # # 計測終了時間
-    # end_time = time.time()
+        # append rect elements to rect_lists
+        for r in packer.rect_list():
+            # index, x, y, w, h
+            rect_lists.append((r[5], r[1], r[2], r[3], r[4]))
+        
+        # Image placement from calculated values
+        output_image = placement_qrcode_images(qrcode_images, rect_lists, bin_width, bin_height)
+        output_image_dir = Parameters.WorkSpacePath["output_image_dir"]
 
-    # process_time = end_time - start_time
+        # # save image to different dir
+        # output_image_path = os.path.join(output_image_dir, "output_{0:02}.png".format(i + 1))
+        # cv2.imwrite(output_image_path, output_image)
+
+        # # save image to same dir
+        output_image_path = os.path.join(output_image_dir, "output.png")
+        cv2.imwrite(output_image_path, output_image)
+
+        # decode
+        decode_start_time = time.time()
+        data = decode(Image.open(output_image_path))
+        decode_end_time = time.time()
+
+        if len(data) == 0:
+            print("Data is none.")
+        else:
+            url = str(data)
+            print("Data is %s" % url[16:38])
+            break
+
+        decode_end_time - decode_start_time
+
+    end_time = time.time()
+
+    process_time = end_time - start_time
+
+    print(process_time)
--- Parameters.py
+++ Parameters.py
@@ -2,11 +2,11 @@
 
 WorkSpacePath = {
     "qrcode_image_path" : "/home/sona/labo/project/packing_qr_image/img/cut_img",
-    "output_image_path" : "/home/sona/labo/project/packing_qr_image/img/output.png"
+    "output_image_dir" : "/home/sona/labo/project/packing_qr_image/img/output_img"
 }
 
 parameters = {
-    "try_steps" : 10,
+    "try_steps" : 1,
     "device_id" : 0,
     "bin_width" : 400,
     "bin_height" : 400
