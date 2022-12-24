579d22a5fd1559d3ce15d2d7956dddc1834c062b
Sona34 <b1019174@fun.ac.jp>
2022-12-24 20:03:32 +0900

    add count time average

--- culc_mean.py
+++ culc_mean.py
@@ -1,16 +1,21 @@
 f = open('packing.log', 'r', encoding='UTF-8')
-
 datalist = f.readlines()
+
 sum_of_time = float()
+sum_of_count = int()
+
 for i, data in enumerate(datalist):
     # print(i, data)
-    if i % 2 == 1:
-        time = data.split(',')
-        print(time[0])
+    if i % 2 == 0:
+        elements = data.strip().split(',')
+        sum_of_count = sum_of_count + int(elements[2])
+    else:
+        time = data.strip().split(',')
         sum_of_time = sum_of_time + float(time[0])
-
 
 num = len(datalist) / 2.0
 average_of_time = sum_of_time / num
-print(sum_of_time, average_of_time)
+average_of_count = sum_of_count / num
+print(sum_of_time, average_of_time, average_of_count)
+
 f.close()
