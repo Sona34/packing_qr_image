b91a56eaeb415ba2e0eaa4e327cde6fe92eefcca
Sona34 <b1019174@fun.ac.jp>
2022-12-24 20:11:59 +0900

    update cluc_mean.py

--- culc_mean.py
+++ culc_mean.py
@@ -5,17 +5,19 @@ sum_of_time = float()
 sum_of_count = int()
 
 for i, data in enumerate(datalist):
-    # print(i, data)
     if i % 2 == 0:
         elements = data.strip().split(',')
-        sum_of_count = sum_of_count + int(elements[2])
     else:
-        time = data.strip().split(',')
-        sum_of_time = sum_of_time + float(time[0])
+        elements = elements + data.strip().split(',')
+
+        sum_of_count = sum_of_count + int(elements[2])
+        sum_of_time = sum_of_time + float(elements[3])
+
+
 
 num = len(datalist) / 2.0
 average_of_time = sum_of_time / num
 average_of_count = sum_of_count / num
-print(sum_of_time, average_of_time, average_of_count)
+print(num, sum_of_time, average_of_time, average_of_count)
 
 f.close()
