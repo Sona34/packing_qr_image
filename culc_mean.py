e01305f39c6789b02b1711bf71e71477deaf3b87
Sona34 <b1019174@fun.ac.jp>
2022-12-24 20:50:15 +0900

    add IDの識別機能

--- culc_mean.py
+++ culc_mean.py
@@ -1,23 +1,34 @@
 f = open('packing.log', 'r', encoding='UTF-8')
 datalist = f.readlines()
 
-sum_of_time = float()
 sum_of_count = int()
+sum_of_time = float()
+log_info = list()
 
 for i, data in enumerate(datalist):
     if i % 2 == 0:
         elements = data.strip().split(',')
     else:
         elements = elements + data.strip().split(',')
+        log_info.append(elements)
+cut_id = input("please type cut_id : ")
+device_id = input("please type device_id : ")
 
-        sum_of_count = sum_of_count + int(elements[2])
-        sum_of_time = sum_of_time + float(elements[3])
+for i, element in enumerate(log_info):
+    # print(i, element)
+    
+    if element[0] == cut_id:
+        # print("cut_id = %s" % cut_id)
+        if element[1] == device_id:
+            # print("device_id = %s" % device_id)
+            sum_of_count = sum_of_count + int(element[2])
+            sum_of_time = sum_of_time + float(element[3])
 
 
 
-num = len(datalist) / 2.0
-average_of_time = sum_of_time / num
-average_of_count = sum_of_count / num
-print(num, sum_of_time, average_of_time, average_of_count)
+n = len(log_info)
+average_of_time = sum_of_time / n
+average_of_count = sum_of_count / n
+print(n, sum_of_time, average_of_time, average_of_count)
 
 f.close()
