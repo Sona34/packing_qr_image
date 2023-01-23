17d7c0eac07b10d6c8349f8169e34e4ca2bc25ad
Sona34 <b1019174@fun.ac.jp>
2023-01-24 05:12:47 +0900

    update data

--- Parameters.py
+++ Parameters.py
@@ -1,31 +1,31 @@
 # for experiments
 
 
-WorkSpacePath = {
-    "QRCODE_IMG_DIR_PATH" : "./img/cut_img/qrcode_5",
-    "OUTPUT_IMG_DIR_PATH" : "./img/output_img/",
-    # "SOURCE_IMG_PATH" : "./img/src_img/fun_qrcode.png",
-}
-
-parameters = {
-    "DEVICE_ID" : 0,
-    "CUT_ID" : "qrcode_5",
-    "BIN_WIDTH" : 400,
-    "BIN_HEIGHT" : 400
-}
-
 # WorkSpacePath = {
-#     "QRCODE_IMG_DIR_PATH" : "./img/cut_img/qrcode_6",
+#     "QRCODE_IMG_DIR_PATH" : "./img/cut_img/qrcode_5",
 #     "OUTPUT_IMG_DIR_PATH" : "./img/output_img/",
 #     # "SOURCE_IMG_PATH" : "./img/src_img/fun_qrcode.png",
 # }
 
 # parameters = {
 #     "DEVICE_ID" : 0,
-#     "CUT_ID" : "qrcode_6",
-#     "BIN_WIDTH" : 600,
-#     "BIN_HEIGHT" : 600
+#     "CUT_ID" : "qrcode_5",
+#     "BIN_WIDTH" : 400,
+#     "BIN_HEIGHT" : 400
 # }
+
+WorkSpacePath = {
+    "QRCODE_IMG_DIR_PATH" : "./img/cut_img/qrcode_6",
+    "OUTPUT_IMG_DIR_PATH" : "./img/output_img/",
+    # "SOURCE_IMG_PATH" : "./img/src_img/fun_qrcode.png",
+}
+
+parameters = {
+    "DEVICE_ID" : 0,
+    "CUT_ID" : "qrcode_6",
+    "BIN_WIDTH" : 600,
+    "BIN_HEIGHT" : 600
+}
 
 # WorkSpacePath = {
 #     "QRCODE_IMG_DIR_PATH" : "./img/cut_img/rand_6",
